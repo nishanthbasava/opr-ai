dc8c3cd240c1cd21db661d94cac05acfc19f73f6
Nishanth Basava <nishanthrbasava@gmail.com>
2026-04-07 19:42:21 -0500

    placeholder loading bar

---
+++ client/src/loadingBar.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+import axios from "axios";
+
+function loadingBar() {
+    const [loading, setLoading] = useState(false);
+
+
+    return 
+    <h1>LOADING BAR</h1>
+    
+
+}
